2a5e4a285c20a829ad9c736463ab23d9834ce13b
ethereal element <vitalik.safonov@list.ru>
2023-09-06 20:05:50 +0300

    int: toggle total button

--- src/scripts/total.js
+++ src/scripts/total.js
@@ -1,5 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector(".order__btn")
+    const button = document.querySelector(".order__btn");
+    const checkbox = document.getElementById("payments");
+    const totalSum = document.getElementById("total__sum");
+    const debitContext = document.querySelector('.debit__text')
+    const debitSub = document.querySelector(".debit__subtitle");
 
-    button.textContent = "FET"
+    debitContext.addEventListener("click", () => {
+        button.textContent = `Оплатить ${totalSum.innerText} сом`;
+    
+        if (debitSub.classList.contains("debit__subtitle-hide")) {
+            button.textContent = 'Заказать';
+        }
+    })
+    
 })
