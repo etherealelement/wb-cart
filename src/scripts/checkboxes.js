af82b014ace81d33b2bc6fbc15e029881188bd1c
ethereal element <vitalik.safonov@list.ru>
2023-09-06 19:23:18 +0300

    init: all checkboxes

--- src/scripts/checkboxes.js
+++ src/scripts/checkboxes.js
@@ -2,6 +2,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const checkSpan = document.querySelector(".select-all");
     const labelCheck = document.querySelector(".checkbox__wrap-spn");
     const checkboxes = document.querySelectorAll(".selected-check");
+    const debitText = document.querySelector(".debit__text");
+    const debitSub = document.querySelector(".debit__subtitle");
+    const checkboxPayment = document.getElementById("payments")
+
 
     labelCheck.addEventListener("click", () => {
         for (const checkbox of checkboxes) {
@@ -20,6 +24,22 @@ window.addEventListener("DOMContentLoaded", () => {
             } else {
                 checkbox.setAttribute("checked", "checked")
             }
+        }
+    })
+
+    debitText.addEventListener("click" , () => {
+        if (debitSub.classList.contains("debit__subtitle-hide")) {
+            debitSub.classList.remove("debit__subtitle-hide")
+        } else {
+            debitSub.classList.add("debit__subtitle-hide")
+        }
+    })
+
+    checkboxPayment.addEventListener("click" , () => {
+        if (debitSub.classList.contains("debit__subtitle-hide")) {
+            debitSub.classList.remove("debit__subtitle-hide")
+        } else {
+            debitSub.classList.add("debit__subtitle-hide")
         }
     })
 })
