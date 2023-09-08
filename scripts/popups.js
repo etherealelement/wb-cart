4b0356ec8982ab832716aa4530ce486e3f7a4fb7
ethereal element <vitalik.safonov@list.ru>
2023-09-08 14:39:04 +0300

    init: popup-payments

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,7 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
     "use strict"
-
-
+    const closeBtn = document.querySelector(".popup-payment__top-spn");
+    const openBtn = document.getElementById("delevery-btn");
+    const overlay = document.querySelector(".overlay");
     
+    openBtn.addEventListener("click", () => {
+        overlay.classList.add("open-overlay")
+    })
+
+    closeBtn.addEventListener("click", () => {
+        overlay.classList.remove("open-overlay")
+    })
+
+    overlay.addEventListener("click", () => {
+        overlay.classList.remove("open-overlay");        
+    })
 
 })
