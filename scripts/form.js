58fe4925baa759ac4473303cb105fc28d478f18a
ethereal element <vitalik.safonov@list.ru>
2023-09-09 16:04:51 +0300

    init: validate email form

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,45 @@
 window.addEventListener("DOMContentLoaded", () => {
-    
-})
+	"use strict";
+	const inputEmail = document.querySelector(".text-3");
+	const inputPhone = document.querySelector(".text-4");
+	const inputInn = document.querySelector(".text-5");
+	const formSpan = document.querySelector(".form__span")
+
+	// email validation
+	inputEmail.addEventListener("blur", function() {
+		const inputEmailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+		if (!inputEmailRegex.test(this.value)) {
+			inputEmail.classList.add("unvalid");
+			formSpan.classList.add("form__span-visible");
+		} else {
+			inputEmail.classList.remove("unvalid");
+			formSpan.classList.remove("form__span-visible");
+		}
+
+		if(this.value.length === 0) {
+			formSpan.innerHTML = "Укажите электронную почту"
+		} else {
+			formSpan.innerHTML = "Проверьте адресс электронной почты"
+		}
+	})
+
+	inputEmail.addEventListener("focus", function(){
+		inputEmail.classList.remove("unvalid")
+	})
+
+	// 
+
+	// phone validation
+
+	inputPhone.addEventListener("blur", function() {
+		const inputPhoneRegex = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+
+		if (!inputPhoneRegex.test(this.value)) {
+			inputPhone.classList.add("unvalid")
+		} else {
+			inputPhone.classList.remove("unvalid")
+		}
+	})
+
+});
