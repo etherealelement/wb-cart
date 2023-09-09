1cb4863066a14067ddc50ebf5054e063c797d946
ethereal element <vitalik.safonov@list.ru>
2023-09-09 21:26:34 +0300

    init: counter

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,3 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
-    
-})
+	const counters = document.querySelectorAll("[data-counter]");
+
+	counters.forEach((counter) => {
+		counter.addEventListener("click", e => {
+            const target = e.target;
+
+            if(target.closest(".order__counter-btn")) {
+                let value = parseInt(target.closest(".order__counter-container").querySelector("input").value);
+
+                if(value === 1) {
+                    value = 1;
+                
+                    target.closest(".order__counter-container").querySelector(".btn-minus").classList.add("btn-disable");
+                }
+
+                if(value === 2) {
+                    value = 2;
+                    target.closest(".order__counter-container").querySelector(".btn-plus").classList.add("btn-disable")
+                    
+                }
+
+                if(target.classList.contains("btn-plus")) {
+                    value++
+                }  else if (target.classList.contains("icon-plus")) {
+                    value++;
+                } else {
+                    --value;
+                }
+
+                
+            
+
+                target.closest(".order__counter-container").querySelector("input").value = value
+            }
+        })
+	});
+});
